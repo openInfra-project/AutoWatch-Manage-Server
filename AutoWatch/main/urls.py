1875f55a5bff20a707b3f2da7a5e15901a2451f1
hufsicehwang <sksmsgkstlr@naver.com>
2021-07-20 18:44:35 +0900

    make&enter page

--- AutoWatch/main/urls.py
+++ AutoWatch/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#  기본 경로 = main/
 urlpatterns = [
    path('', views.main),
+   path('makeroom/',views.makeroom),
+   path('enteroom/',views.enteroom),
 ]
--- AutoWatch/main/views.py
+++ AutoWatch/main/views.py
@@ -1,5 +1,12 @@
+from django.http import response
 from django.shortcuts import render
 
 # Create your views here.
 def main(request):
     return render(request,'main.html')
+
+def makeroom(request):
+    return render(request,'makeroom.html')    
+    
+def enteroom(request):
+    return render(request,'enteroom.html')    
