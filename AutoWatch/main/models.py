d7d1210b93d009d87de4fad95c1169cbc27720bb
hufsicehwang <sksmsgkstlr@naver.com>
2021-07-29 20:13:10 +0900

    방 생성 구현

--- AutoWatch/main/models.py
+++ AutoWatch/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    room_name = models.CharField(max_length=64, verbose_name="방 이름")
+    room_password = models.CharField(max_length=64, verbose_name="방 비밀번호")
+    file = models.FileField(verbose_name="파일")
+
+    def __str__(self):
+        return self.room_name
+
+    class Meta:
+        db_table = 'room'
+        verbose_name = 'Room 명단'
+        verbose_name_plural = 'Room 명단'
--- AutoWatch/main/views.py
+++ AutoWatch/main/views.py
@@ -13,21 +13,34 @@ def main(request):
         return render(request,'login.html')
 
 
-
-
-
 def makeroom(request):
     res_data={}
     user_session = request.session.get('user')
     if user_session:
         user = User.objects.get(pk=user_session)
-        res_data['username'] = user.username
+        res_data['username'] = user.username                # 로그인 체크
         if request.method == 'GET':
             return render(request,'makeroom.html',res_data)
         elif request.method == 'POST':
-            room_name = request.POST
-    else:
-        return render(request,'login.html')
+            room_name = request.POST.get('room-name',None)
+            room_password = request.POST.get('room-password',None)
+            file = request.POST.get('file',None)
+            study = request.POST.getlist('study')
+            exam = request.POST.getlist('exam')
+            
+            if (len(room_name) > 7):
+                res_data['name_error'] = '방 이름을 생성해 주세요.'
+            elif not(room_password):
+                res_data['password_error'] = '비밀번호를 생성해 주세요.'
+            elif not(study or exam):
+                res_data['mode_error'] = 'Mode를 선택해 주세요.'    
+            else:
+                if (exam and not(file)):
+                    res_data['file_error'] = 'Exam Mode는 명단 첨부가 필수 입니다.'
+                    return render(request,'makeroom.html',res_data)  # room 정보 비정상 일시
+                else:  # 정상적으로 room 정보 기입시   
+                    return render(request,'makeroom.html')
+            return render(request,'makeroom.html',res_data)  # room 정보 비정상 일시
     
 
     
