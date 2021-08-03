96f60afe860458c1d0afa34824d56b49adbc028e
hufsicehwang <sksmsgkstlr@naver.com>
2021-08-04 02:49:46 +0900

    mypage 오류 수정

--- AutoWatch/main/views.py
+++ AutoWatch/main/views.py
@@ -46,57 +46,57 @@ def makeroom(request):
         res_data['string'] = ''.join(random.choice(string.ascii_uppercase + string.digits)for _ in range(7))  # 랜덤 문자열 생성
         res_data['userimg'] = fs.url(user.image)
 
-        if res_data['userimg'] == "/media/":               # 이미지 체크
+        if res_data['userimg'] == "/media/":               # 이미지가 있는지 체크
             res_data['check'] = 0
-            return render(request,'makeroom.html',res_data)
         else:
             res_data['check'] = 1
-            if request.method == 'GET':
-                return render(request,'makeroom.html',res_data)
-            elif request.method == 'POST':
-                post_type = request.POST.get('user-img-btn')
-                if post_type == "모달":  # 버튼 값을 읽어 POST 구분, 이미지 변경 POST
-                    # 이미지 변경 저장
-                    userimage = request.FILES['user-img-change']
-                    res_data['userimg'] = fs.url(userimage)
-                    user.image = userimage
-                    user.save()
-                    return render(request,'makeroom.html',res_data) 
-                else:  # Room create POST
-                    # makeroom POST 값
-                    room_name = request.POST.get('room-name',None)
-                    room_password = request.POST.get('room-password',None)
-                    file = request.POST.get('file',None)
-                    study = request.POST.getlist('study')
-                    exam = request.POST.getlist('exam')
-                    maker = user.email
 
-                    # makeroom POST 값 저장
-                    request.session['room_name'] = room_name
-                    request.session['file'] = file
-                    request.session['study'] = study
-                    request.session['exam'] = exam
-                    request.session['maker'] = maker
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                    if (len(room_name) > 7):
-                        res_data['name_error'] = '방 이름을 생성해 주세요.'
-                    elif not(room_password):
-                        res_data['password_error'] = '비밀번호를 생성해 주세요.'
-                    elif not(study or exam):
-                        res_data['mode_error'] = 'Mode를 선택해 주세요.'    
-                    else:
-                        if (exam and not(file)):
-                            res_data['mode_error'] = 'Exam Mode는 명단 첨부가 필수 입니다.'
-                            return render(request,'makeroom.html',res_data)  # room 정보 비정상 일시
-                        else:  # 정상적으로 room 정보 기입시
-                            if study and not(exam):     # mode를 db에 저장
-                                mode = 'STUDY'
-                            elif exam and not(study):
-                                mode = 'EXAM'
-                            room = Room(room_name=room_name, room_password=room_password, file=file,mode=mode ,maker=maker) # db에 room 정보 저장
-                            room.save()   
-                            return redirect('/main/makeroom/success')
-                    return render(request,'makeroom.html',res_data)  # room 정보 비정상 일시
+        if request.method == 'GET':
+            return render(request,'makeroom.html',res_data)
+        elif request.method == 'POST':
+            post_type = request.POST.get('user-img-btn')
+            if post_type == "모달":  # 버튼 값을 읽어 POST 구분, 이미지 변경 POST
+                # 이미지 변경 저장
+                userimage = request.FILES['user-img-change']
+                res_data['userimg'] = fs.url(userimage)
+                user.image = userimage
+                user.save()
+                return render(request,'makeroom.html',res_data) 
+            else:  # Room create POST
+                # makeroom POST 값
+                room_name = request.POST.get('room-name',None)
+                room_password = request.POST.get('room-password',None)
+                file = request.POST.get('file',None)
+                study = request.POST.getlist('study')
+                exam = request.POST.getlist('exam')
+                maker = user.email
+
+                # makeroom POST 값 저장
+                request.session['room_name'] = room_name
+                request.session['file'] = file
+                request.session['study'] = study
+                request.session['exam'] = exam
+                request.session['maker'] = maker
+
+                if (len(room_name) > 7):
+                    res_data['name_error'] = '방 이름을 생성해 주세요.'
+                elif not(room_password):
+                    res_data['password_error'] = '비밀번호를 생성해 주세요.'
+                elif not(study or exam):
+                    res_data['mode_error'] = 'Mode를 선택해 주세요.'    
+                else:
+                    if (exam and not(file)):
+                        res_data['mode_error'] = 'Exam Mode는 명단 첨부가 필수 입니다.'
+                        return render(request,'makeroom.html',res_data)  # room 정보 비정상 일시
+                    else:  # 정상적으로 room 정보 기입시
+                        if study and not(exam):     # mode를 db에 저장
+                            mode = 'STUDY'
+                        elif exam and not(study):
+                            mode = 'EXAM'
+                        room = Room(room_name=room_name, room_password=room_password, file=file,mode=mode ,maker=maker) # db에 room 정보 저장
+                        room.save()   
+                        return redirect('/main/makeroom/success')
+                return render(request,'makeroom.html',res_data)  # room 정보 비정상 일시
     else:
         return redirect('/login')
     
@@ -121,22 +121,21 @@ def make_success(request):
 
         if res_data['userimg'] == "/media/":               # 이미지 체크
             res_data['check'] = 0
-            return render(request,'make_success.html',res_data)
         else:
             res_data['check'] = 1  
-            if request.method == 'GET':
-                return render(request,'make_success.html',res_data)
-            elif request.method == 'POST':
-                post_type = request.POST.get('user-img-btn')
-                if post_type == "모달":  # 버튼 값을 읽어 POST 구분, 이미지 변경 POST
-                    # 이미지 변경 저장
-                    userimage = request.FILES['user-img-change']
-                    res_data['userimg'] = fs.url(userimage)
-                    user.image = userimage
-                    user.save()
-                    return render(request,'make_success.html',res_data) 
-                else:  #  Room 입장 POST
-                    return render(request,'ssimong.html')
+        if request.method == 'GET':
+            return render(request,'make_success.html',res_data)
+        elif request.method == 'POST':
+            post_type = request.POST.get('user-img-btn')
+            if post_type == "모달":  # 버튼 값을 읽어 POST 구분, 이미지 변경 POST
+                # 이미지 변경 저장
+                userimage = request.FILES['user-img-change']
+                res_data['userimg'] = fs.url(userimage)
+                user.image = userimage
+                user.save()
+                return render(request,'make_success.html',res_data) 
+            else:  #  Room 입장 POST
+                return render(request,'ssimong.html')
     else:
         return redirect('/login')
 
@@ -153,43 +152,42 @@ def enteroom(request):
 
         if res_data['userimg'] == "/media/":               # 이미지 체크
             res_data['check'] = 0
-            return render(request,'enteroom.html',res_data)
         else:
             res_data['check'] = 1
             
-            if request.method == 'GET':
-                return render(request,'enteroom.html',res_data)
-            elif request.method == 'POST':
-                post_type = request.POST.get('user-img-btn')
-                if post_type == "모달":  # 버튼 값을 읽어 POST 구분, 이미지 변경 POST
-                    # 이미지 변경 저장
-                    userimage = request.FILES['user-img-change']
-                    res_data['userimg'] = fs.url(userimage)
-                    user.image = userimage
-                    user.save()
-                    return render(request,'enteroom.html',res_data) 
-                else:  #  Room 입장 POST
-                    room_name = request.POST.get('room_name')
-                    room_password = request.POST.get('room_password')
-                    if not(room_name):
-                        res_data['name_error'] = 'Room 이름을 입력하세요.'
-                    elif not(room_password):
-                        res_data['password_error'] = 'Room 비밀번호를 입력하세요.'
-                    elif not(room_name and room_password):
-                        res_data['all_error'] = '모든 값을 입력하세요.'
-                    else:
-                        try:
-                            room = Room.objects.get(room_name=room_name) # 필드명 = 값 이면 Room 객체 생성
-                        except Room.DoesNotExist:
-                            res_data['error'] = '존재하지 않는 Room 입니다.'    # room이 없는 예외 처리
-                            return render(request,'enteroom.html',res_data)
+        if request.method == 'GET':
+            return render(request,'enteroom.html',res_data)
+        elif request.method == 'POST':
+            post_type = request.POST.get('user-img-btn')
+            if post_type == "모달":  # 버튼 값을 읽어 POST 구분, 이미지 변경 POST
+                # 이미지 변경 저장
+                userimage = request.FILES['user-img-change']
+                res_data['userimg'] = fs.url(userimage)
+                user.image = userimage
+                user.save()
+                return render(request,'enteroom.html',res_data) 
+            else:  #  Room 입장 POST
+                room_name = request.POST.get('room_name')
+                room_password = request.POST.get('room_password')
+                if not(room_name):
+                    res_data['name_error'] = 'Room 이름을 입력하세요.'
+                elif not(room_password):
+                    res_data['password_error'] = 'Room 비밀번호를 입력하세요.'
+                elif not(room_name and room_password):
+                    res_data['all_error'] = '모든 값을 입력하세요.'
+                else:
+                    try:
+                        room = Room.objects.get(room_name=room_name) # 필드명 = 값 이면 Room 객체 생성
+                    except Room.DoesNotExist:
+                        res_data['error'] = '존재하지 않는 Room 입니다.'    # room이 없는 예외 처리
+                        return render(request,'enteroom.html',res_data)
 
-                        db_password = room.room_password
-                        if db_password == room_password:     # room 정상 입장
-                            return render(request, 'ssimong.html')
-                        else:
-                            res_data['error'] = '비밀번호가 틀렸습니다.'
-                            return render(request, 'enteroom.html',res_data)
-                    return render(request,'enteroom.html',res_data)  # room 정보 비정상 일시
+                    db_password = room.room_password
+                    if db_password == room_password:     # room 정상 입장
+                        return render(request, 'ssimong.html')
+                    else:
+                        res_data['error'] = '비밀번호가 틀렸습니다.'
+                        return render(request, 'enteroom.html',res_data)
+                return render(request,'enteroom.html',res_data)  # room 정보 비정상 일시
     else:
         return redirect('/login')
